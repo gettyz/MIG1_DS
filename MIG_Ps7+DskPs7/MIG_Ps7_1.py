1aa083a8e3f2a52c75d2b95e55065abc5b6853a1
gettyz <144375933+gettyz@users.noreply.github.com>
2023-10-18 12:55:23 +0300

    Update MIG_Ps7_1.py

--- MIG_Ps7+DskPs7/MIG_Ps7_1.py
+++ MIG_Ps7+DskPs7/MIG_Ps7_1.py
@@ -1,5 +1,11 @@
+# Импортируем библиотеку matplotlib для визуализации данных
 import matplotlib.pyplot as plt
+
+# Инициализируем начальные значения для чисел Трибоначчи (0, 0, 1)
 tribonacci_numbers_cache = [0, 0, 1]
+
+# Заполняем список tribonacci_numbers_cache числами Трибоначчи до 100-го числа
+# Здесь число Трибоначчи определяется как сумма трех предыдущих чисел в последовательности
 for i in range(3, 100):
     tribonacci_numbers_cache.append(
         tribonacci_numbers_cache[i - 1]
@@ -7,20 +13,38 @@ for i in range(3, 100):
         + tribonacci_numbers_cache[i - 3]
     )
 
+# Уже заданная нами конкретная последовательность чисел Трибоначчи, значения которых нам нужно найти
 tribonacci_numbers = [73, 10, 4, 15, 20, 7]
-# список для сохранения самих чисел трибонначи
+
+# Создаем пустой список для сохранения значений заданных чисел Трибоначчи
 tribonacci_numbers_value = []
+
+# Поочередно перебираем каждое число из списка tribonacci_numbers
 for number in tribonacci_numbers:
-    # просто получаем значения из кеша, не делая никаких вычислений
+    # Получаем значение текущего числа из нашего ранее созданного кэша чисел Трибоначчи
     tribonacci_numbers_value.append(tribonacci_numbers_cache[number - 1])
 
-print(tribonacci_numbers_value)
+# Выводим значения чисел Трибоначчи из списка
+print('список значений Трибоначчи: ', tribonacci_numbers_value)
 
-# Визуализация
+# Начинаем визуализировать наши данные
 plt.figure(figsize=(10, 5))
+
+# Используем функцию plot для создания графика, где x-осью являются числа Трибоначчи, а y-осью - их значения
+# Tuning the parameters like marker, linestyle and color for better visibility
 plt.plot(tribonacci_numbers, tribonacci_numbers_value, marker='o', linestyle='-', color='b')
+
+# Задаем заголовок для графика
 plt.title('Значения Трибоначчиевых чисел')
+
+# Задаем название для оси X
 plt.xlabel('Трибоначчиевы числа')
+
+# Задаем название для оси Y
 plt.ylabel('Значения')
+
+# Добавляем сетку для удобства чтения графика
 plt.grid(True)
+
+# Выводим график на экран
 plt.show()
